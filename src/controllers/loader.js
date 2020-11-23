f3962eb56a7cd1eef4c51cb57d8aea03fc694976
avistocco6 <a.vistocco6@studenti.unisa.it>
2020-11-23 13:13:06 +0100

    controller and model first communication

---
+++ src/controllers/loader.js
@@ -0,0 +1,32 @@
+function loadMaterials() {
+    jQuery.ajax({
+        type: "POST",
+        url: '../src/models/loader.php',
+        dataType: 'json',
+        data: {functionname: "loadModels", arguments: []},
+
+        success: function (obj, textstatus) {
+            if( !('error' in obj) ) {
+                let data = JSON.parse(obj.result);
+                let staticHtml = $("#materials-row-template").html();
+
+                $.each(data, function (index, obj) {
+                  let row = staticHtml;
+                  console.log(obj.name);
+                  row = row.replace(/{Material}/ig, obj.name);
+                  $('#materials-rows').append(row);
+                });
+
+                /* When empty material */
+                if (data.length === 0) {
+                    $("tfoot :first-child").hide();
+                    $("tfoot").html('<tr><th>No records</th></tr>');
+                }
+            }
+            else {
+                console.log(obj.error);
+                alert("Impossible to load materials");
+            }
+        }
+    });
+}
---
+++ src/models/loader.php
@@ -0,0 +1,26 @@
+<?php
+foreach (glob("..\items\*.php") as $filename)
+{
+    include $filename;
+}
+
+header('Content-Type: application\json');
+
+$aResult = array();
+
+if( !isset($_POST['functionname']) ) { $aResult['error'] = 'No function name!'; }
+
+if( !isset($aResult['error']) ) {
+
+    switch($_POST['functionname']) {
+        case 'loadModels':
+            $aResult['result'] = Material::get_materials();
+            break;
+        default:
+            $aResult['error'] = 'Not found function '.$_POST['functionname'].'!';
+            break;
+    }
+
+}
+
+echo json_encode($aResult);
