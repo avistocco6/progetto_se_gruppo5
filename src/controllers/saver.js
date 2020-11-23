750f76dfeb23973ca83834e0fd1899cf1c5f5fd4
SimonaSorgente <73382491+SimonaSorgente@users.noreply.github.com>
2020-11-23 21:39:40 +0100

    update controllers

---
+++ src/controllers/saver.js
@@ -0,0 +1,53 @@
+function addMaterial() {
+    let name = document.getElementById("material-name").value;
+    let material = '{"name":' + '"' + name + '"}';
+    jQuery.ajax({
+        type: "POST",
+        url: '../models/saver.php',
+        dataType: 'json',
+        data: {functionname: "saveModel", arguments: [material]},
+
+        success: function (obj, textstatus) {
+            if( !('error' in obj) ) {
+                console.log(obj);
+                if(obj == true){
+                    alert("Successfully saved!")
+                }
+                else{
+                    alert("Error during saving!")
+                }
+            }
+            else {
+                console.log(obj.error);
+                alert("Impossible to save material!");
+            }
+        }
+    });
+}
+
+function addSkill() {
+    let name = document.getElementById("skill-name").value;
+    let skill = '{"name":' + '"' + name + '"}';
+    jQuery.ajax({
+        type: "POST",
+        url: '../models/saver.php',
+        dataType: 'json',
+        data: {functionname: "saveModel", arguments: [skill]},
+
+        success: function (obj, textstatus) {
+            if( !('error' in obj) ) {
+                console.log(obj);
+                if(obj == true){
+                    alert("Successfully saved!")
+                }
+                else{
+                    alert("Error during saving!")
+                }
+            }
+            else {
+                console.log(obj.error);
+                alert("Impossible to save skill!");
+            }
+        }
+    });
+}
