66e0494a69a69d61dfe6b29824c8ea4c8e9e96bf
anapoli98 <a.napoli98@gmail.com>
2020-11-24 21:51:09 +0100

    load Selected functions and templates added to selected.html (mock-up 2)

--- src/controllers/loader.js
+++ src/controllers/loader.js
@@ -64,3 +64,58 @@ function loadSkills() {
     });
 }
 
+function loadSelected() {
+    jQuery.ajax({
+        type: "POST",
+        url: '../models/loader.php',
+        dataType: 'json',
+        data: {functionname: "loadSelected", arguments: []},
+
+        success: function (obj, textstatus) {
+            if( !('error' in obj) ) {
+                let data = JSON.parse(obj.result);
+                let staticHtml1 = $("#workspace-row-template").html();
+                let staticHtml2 = $("#intervDescription-row-template").html();
+                let staticHtml3 = $("#skillsNeeded-row-template").html();
+
+                $.each(data, function (index, obj) {
+                /* Workspace notes */
+                    let row1 = staticHtml1;
+                    row1 = row1.replace(/{Workspace Notes}/ig, obj.name);
+                    $('#workspace-rows').append(row1);
+
+                /* Intervention Description */
+                    let row2 = staticHtml2;
+                    row2 = row2.replace(/{Intervention Description}/ig, obj.name);
+                    $('#intervDescription-rows').append(row2);
+
+                /* Intervention Description */
+                    let row3 = staticHtml3;
+                    row3 = row3.replace(/{Skills Needed}/ig, obj.name);
+                    $('#skillsNeeded-rows').append(row2);
+                });
+
+                /* When empty workspace notes, intervention description or  */
+                if (data === null) {
+                    let row1 = staticHtml1;
+                    row1 = row1.replace(/{Workspace Notes}/ig, "There are no workspace notes");
+                    $('#workspace-rows').append(row1);
+
+                    let row2 = staticHtml2;
+                    row2 = row2.replace(/{Intervention Description}/ig, "There are no Intervention Description");
+                    $('#intervDescription-rows').append(row2);
+
+                    let row3 = staticHtml3;
+                    row3 = row3.replace(/{Skills Needed}/ig, "There are no Skills Needed");
+                    $('#skillsNeeded-rows').append(row3);
+                }
+            }
+            else {
+                console.log(obj.error);
+                alert("Impossible to load the tables");
+            }
+
+        }
+    });
+}
+
