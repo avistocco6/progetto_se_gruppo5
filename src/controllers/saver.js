99c0286d127ddb77969a5a468fa872bdf4309d78
anapoli98 <a.napoli98@gmail.com>
2020-11-26 10:11:45 +0100

    load and save of procedures, sites and typology

--- src/controllers/saver.js
+++ src/controllers/saver.js
@@ -104,3 +104,84 @@ function addActivity() {
         }
     });
 }
+
+function addSite() {
+    let name = document.getElementById("site-name").value;
+    let site = '{"name":' + '"' + name + '"}';
+    jQuery.ajax({
+        type: "POST",
+        url: '../models/saver.php',
+        dataType: 'json',
+        data: {functionname: "saveModel", arguments: [site]},
+
+        success: function (obj, textstatus) {
+            if( !('error' in obj) ) {
+                console.log(obj);
+                if(obj == true){
+                    alert("Successfully saved!")
+                }
+                else{
+                    alert("Error during saving!")
+                }
+            }
+            else {
+                console.log(obj.error);
+                alert("Impossible to save site!");
+            }
+        }
+    });
+}
+
+function addProcedure() {
+    let name = document.getElementById("procedure-name").value;
+    let procedure = '{"name":' + '"' + name + '"}';
+    jQuery.ajax({
+        type: "POST",
+        url: '../models/saver.php',
+        dataType: 'json',
+        data: {functionname: "saveModel", arguments: [procedure]},
+
+        success: function (obj, textstatus) {
+            if( !('error' in obj) ) {
+                console.log(obj);
+                if(obj == true){
+                    alert("Successfully saved!")
+                }
+                else{
+                    alert("Error during saving!")
+                }
+            }
+            else {
+                console.log(obj.error);
+                alert("Impossible to save procedure!");
+            }
+        }
+    });
+}
+
+function addTypology() {
+    let name = document.getElementById("typology-name").value;
+    let typology = '{"name":' + '"' + name + '"}';
+    jQuery.ajax({
+        type: "POST",
+        url: '../models/saver.php',
+        dataType: 'json',
+        data: {functionname: "saveModel", arguments: [typology]},
+
+        success: function (obj, textstatus) {
+            if( !('error' in obj) ) {
+                console.log(obj);
+                if(obj == true){
+                    alert("Successfully saved!")
+                }
+                else{
+                    alert("Error during saving!")
+                }
+            }
+            else {
+                console.log(obj.error);
+                alert("Impossible to save typology!");
+            }
+        }
+    });
+}
