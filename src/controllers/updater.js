730e55c93030d20d986d5166fa8e691e34a4a659
anapoli98 <a.napoli98@gmail.com>
2020-11-27 16:14:39 +0100

    updater functions

--- src/controllers/updater.js
+++ src/controllers/updater.js
@@ -22,3 +22,108 @@ function updateMaterial(id, name) {
         }
     });
 }
+
+function updateSkill(id, name) {
+    let json_string = '{"id": ' + id + '"name":' + '"' + name + '"}';
+    jQuery.ajax({
+        type: "POST",
+        url: '../models/updater.php',
+        dataType: 'json',
+        data: {functionname: "updateSkill", arguments: [json_string]},
+
+        success: function (obj, textstatus) {
+            if( !('error' in obj) ) {
+                console.log(obj);
+                if(obj == true){
+                    alert("Successfully updated!")
+                }
+                else{
+                    alert("Error during update!")
+                }
+            }
+            else {
+                console.log(obj.error);
+                alert("Impossible to update skills!");
+            }
+        }
+    });
+}
+
+function updateSite(id, name) {
+    let json_string = '{"id": ' + id + '"name":' + '"' + name + '"}';
+    jQuery.ajax({
+        type: "POST",
+        url: '../models/updater.php',
+        dataType: 'json',
+        data: {functionname: "updateSite", arguments: [json_string]},
+
+        success: function (obj, textstatus) {
+            if( !('error' in obj) ) {
+                console.log(obj);
+                if(obj == true){
+                    alert("Successfully updated!")
+                }
+                else{
+                    alert("Error during update!")
+                }
+            }
+            else {
+                console.log(obj.error);
+                alert("Impossible to update sites!");
+            }
+        }
+    });
+}
+
+function updateTypology(id, name) {
+    let json_string = '{"id": ' + id + '"name":' + '"' + name + '"}';
+    jQuery.ajax({
+        type: "POST",
+        url: '../models/updater.php',
+        dataType: 'json',
+        data: {functionname: "updateTypology", arguments: [json_string]},
+
+        success: function (obj, textstatus) {
+            if( !('error' in obj) ) {
+                console.log(obj);
+                if(obj == true){
+                    alert("Successfully updated!")
+                }
+                else{
+                    alert("Error during update!")
+                }
+            }
+            else {
+                console.log(obj.error);
+                alert("Impossible to update typology!");
+            }
+        }
+    });
+}
+
+function updateProcedure(id, name) {
+    let json_string = '{"id": ' + id + '"name":' + '"' + name + '"}';
+    jQuery.ajax({
+        type: "POST",
+        url: '../models/updater.php',
+        dataType: 'json',
+        data: {functionname: "updateProcedure", arguments: [json_string]},
+
+        success: function (obj, textstatus) {
+            if( !('error' in obj) ) {
+                console.log(obj);
+                if(obj == true){
+                    alert("Successfully updated!")
+                }
+                else{
+                    alert("Error during update!")
+                }
+            }
+            else {
+                console.log(obj.error);
+                alert("Impossible to update procedure!");
+            }
+        }
+    });
+}
+
