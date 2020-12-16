245226ef82692cefd5e62dcbea07ad3356d34580
antons4 <74857318+antons4@users.noreply.github.com>
2020-12-16 10:25:18 +0100

    fixed html & css

--- src/views/selecthours.php
+++ src/views/selecthours.php
@@ -1,203 +1,210 @@
 <!DOCTYPE html>
 <html>
-
 <head>
-  <meta charset="UTF-8">
-  <title>Choose Maintainer</title>
-        <!--The system must allow selecting among the days of the week that the Maintainer has  availability and show
-         the following information: week number, date (selected day), activity to assign (activity ID, area, typology,
-          estimated intervention time), workspace  notes, maintainer name, maintainer availability percentage,
-          maintainer competencies  compliance, availability (in minutes for each hour of his workday, e.g. 8:00-9:00,
-          9:00- 10:00).
-          At this point, the system must allow to select the slot of availability time (to assign the schedule in
-          minutes) in which the maintenance activity will be assigned and program the activity. -->
+<meta charset="UTF-8">
+<title>Choose Maintainer</title>
+    <!--The system must allow selecting among the days of the week that the Maintainer has  availability and show
+     the following information: week number, date (selected day), activity to assign (activity ID, area, typology,
+      estimated intervention time), workspace  notes, maintainer name, maintainer availability percentage,
+      maintainer competencies  compliance, availability (in minutes for each hour of his workday, e.g. 8:00-9:00,
+      9:00- 10:00).
+      At this point, the system must allow to select the slot of availability time (to assign the schedule in
+      minutes) in which the maintenance activity will be assigned and program the activity. -->
 
-          <meta charset="utf-8"/>
-          <meta name="author" content="Team 5"/>
-          <meta name="description" content="Web application for maintenance activies."/>
-          <link rel="preconnect" href="https://fonts.gstatic.com">
-          <link href="https://fonts.googleapis.com/css2?family=Goldman&display=swap" rel="stylesheet">
-          <link rel="stylesheet" type="text/css" href="showstyle.css"/>
-          <link rel="stylesheet" type="text/css" href="stylesheet.css"/>
-        </head>
-        <body>
-         <div class="row">
-          <div class="header">
-          </div>
-          <div class="header" >
-            <h1> Choose Maintainer </h1>
-          </div> 
-          <div class="header">
-            <!--Script per cambiare lo stile della pagina in caso di accesso di un utente-->
-          <?php
-          session_start();
-          if(!empty($_SESSION["username"])){
-            $username = $_SESSION["username"];
-            $html = <<< HTML
-            <p style="text-align: center;"> $username </p>
-            <hr>
-            <p style="text-align: center;"><a href="logout.php"> Logout </a><p>
-            HTML;
-            echo $html;
-          }
-          ?>  
-          </div> 
-        </div>
-        <div class="column">
-          <div class="header2" style="margin: 0;">
-            <div id= "week">
-              <p> Week n° </p>
-            </div>
-            <div id="number">
-              <p id="numWeek"></p>
-            </div>
-            <div id= "day">
-              <p> Day </p>
-            </div>
-            <div id= "dayavail">
-              <p id="dayName">  </p>
-            </div>
-            <div id="activity">
-              <p>Activity to assign</p>
-            </div>
-            <div id="name">
-              <p id="activityName"></p>
-            </div>
-            
-          </div>
-        </div>
-        <table class="tab">
-          <tr>
-            <th>Workspace Notes</th>
-          </tr>
-          <thead>
-            <template id="workspace-row-template">
-              <tr>
-                <td>{Workspace Notes}</td>
-              </tr>
-            </template>
-          </thead>
+      <meta charset="utf-8"/>
+      <meta name="author" content="Team 5"/>
+      <meta name="description" content="Web application for maintenance activies."/>
+      <link rel="preconnect" href="https://fonts.gstatic.com">
+      <link href="https://fonts.googleapis.com/css2?family=Goldman&display=swap" rel="stylesheet">
+      <link rel="stylesheet" type="text/css" href="showstyle.css"/>
+      <link rel="stylesheet" type="text/css" href="stylesheet.css"/>
+  </head>
+  <body>
+  	<div class="row">
+  		<div class="header">
+  		</div>
+  		<div class="header" >
+  			<h1> Choose Maintainer </h1>
+  		</div> 
+  		<div class="header">
+  			<!--Script per cambiare lo stile della pagina in caso di accesso di un utente-->
+			<?php
+			session_start();
+			if(!empty($_SESSION["username"])){
+				$username = $_SESSION["username"];
+				$html = <<< HTML
+				<p style="text-align: center;"> $username </p>
+				<hr>
+				<p style="text-align: center;"><a href="logout.php"> Logout </a></p>
+				HTML;
+				echo $html;
+			}
+			?>   
+  		</div>  
+  	</div>
 
-          <tbody id="workspace-rows">
+  	<div class="column">
+  		<div class="header2" style="margin: 0;">
+  			<div id= "week">
+  				<p> Week n° </p>
+  			</div>
+  			<div id="number">
+  				<p id="numWeek"></p>
+  			</div>
+  			<div id= "day">
+  				<p> Day </p>
+  			</div>
+  			<div id= "dayavail">
+  				<p id="dayName">  </p>
+  			</div>
+  			<div id="activity">
+  				<p>Activity to assign</p>
+  			</div>
+  			<div id="name">
+  				<p id="activityName"></p>
+  			</div>
 
-          </tbody>
-        </table>
-        <div id="column" class="column">
-          <div class="header3" style="margin: 0;">
-            <div id= "mainAvail">
-              <p> <b>MAINTAINER AVAILABILITY</b></p>
-            </div>
-          </div>
-        </div>
-        
+  		</div>
+  		<div class="content">
+  			<table class="tab1">
+  				<tr>
+  					<th>Workspace Notes</th>
+  				</tr>
+  				<thead>
+  					<template id="workspace-row-template">
+  						<tr>
+  							<td>{Workspace Notes}</td>
+  						</tr>
+  					</template>
+  				</thead>
 
-        <table id="maint-availab" style=" width:70%; position: absolute; top:320; left: 25.7%" >
-          <thead>
+  				<tbody id="workspace-rows" >
 
-            <tr>
-              <th>Maintainer</th>
-              <th>Availab. 8:00-9:00</th>
-              <th>Availab. 9:00-10:00</th>
-              <th>Availab. 10:00-11:00</th>
-              <th>Availab. 11:00-12:00</th>
-              <th>Availab. 14:00-15:00</th>
-              <th>Availab. 15:00-16:00</th>
-              <th>Availab. 16:00-17:00</th>
-            </tr>
-            <template id="maint-availab-template">
-              <tr>
-                <td style="text-align:center;">{MainName}</td>
-                <td style="text-align:center;">
-                  <button type="button">{Availab8}</button>
-                </td>
-                <td style="text-align:center;">
-                  <button type="button">{Availab9}</button>
-                </td>
-                <td style="text-align:center;">
-                  <button type="button">{Availab10}</button>
-                </td>
-                <td style="text-align:center;">
-                  <button type="button">{Availab11}</button>
-                </td>
-                <td style="text-align:center;">
-                  <button type="button">{Availab14}</button>
-                </td >
-                <td style="text-align:center;">
-                  <button type="button">{Availab15}</button>
-                </td>
-                <td style="text-align:center;">
-                  <button type="button">{Availab16}</button>
-                </td>
+  				</tbody>
+  			</table>
+  			<a href="#"a><input type="button" value="send" id="send" name="send" style="float: left;visibility: hidden; width: 100px;
+  				height: 50px;">
+  			</a>
+  			<table id="maint-availab">
+  				<thead>
+  					<tr>
+  						<th colspan="9" style="text-align: center;"><h2>Maintainer Availability</h2></th>
+  					</tr>
+  					<tr>
+  						<th>Maintainer</th>
+  						<th>Availab. 8:00-9:00</th>
+  						<th>Availab. 9:00-10:00</th>
+  						<th>Availab. 10:00-11:00</th>
+  						<th>Availab. 11:00-12:00</th>
+  						<th>Availab. 14:00-15:00</th>
+  						<th>Availab. 15:00-16:00</th>
+  						<th>Availab. 16:00-17:00</th>
+  					</tr>
+  					<template id="maint-availab-template">
+  						<tr>
+  							<td style="text-align:center;">{MainName}</td>
+  							<td style="text-align:center;">
+  								<button type="button" onclick="changeColor(this);">{Availab8}</button>
+  							</td>
+  							<td style="text-align:center;">
+  								<button type="button" onclick="changeColor(this);">{Availab9}</button>
+  							</td>
+  							<td style="text-align:center;">
+  								<button type="button" onclick="changeColor(this);">{Availab10}</button>
+  							</td>
+  							<td style="text-align:center;">
+  								<button type="button" onclick="changeColor(this);">{Availab11}</button>
+  							</td>
+  							<td style="text-align:center;">
+  								<button type="button" onclick="changeColor(this);">{Availab14}</button>
+  							</td >
+  							<td style="text-align:center;">
+  								<button type="button" onclick="changeColor(this);">{Availab15}</button>
+  							</td>
+  							<td style="text-align:center;">
+  								<button type="button" onclick="changeColor(this);">{Availab16}</button>
+  							</td>
 
-                </tr>
-              </template>
-            </thead>
-            <tbody id="maint-availab-rows">
+  						</tr>
+  					</template>
+  				</thead>
+  				<tbody id="maint-availab-rows">
 
-            </tbody>
-          </table>
-          <br>
-          <br>
-          <br>
-          <a href="#"a><input type="button" value="confirm" onclick="review();" id="confirm" name="confirm" style="margin: 10px; width: 100px;
-          height: 50px;top: 0; ">
-          </a>
-          <br>
-          <a href="#"a><input type="button" value="send" id="send" name="send" style="display:none; float:right;margin: 10px; width: 100px;
-          height: 50px;top: 0;">
-          </a>
-        <div class="footer" style="position: fixed;">
-          <h2>Team 5</h2>
-        </div>
+  				</tbody>
+  			</table>
+  			<br>
+  			<div class="button" style="position: center;">
+  				<a href="#"a><input type="button" value="confirm" disabled onclick="review();" id="confirm" name="confirm" style="float:left; height: 50px; width: 100px;">
+  				</a>
+  				
+  		</div>
+  	</div>
+  </div>
+  <br>
+  <br>
+  <br>
 
-        <!-- jQuery -->
-        <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.0/jquery.min.js"></script>
+  <br>
+  
+</a>
+<div class="footer" style="position: fixed;">
+	<h2>Team 5</h2>
+</div>
 
-        <!-- Custom JS -->
-        <script type="text/javascript" src="../controllers/loader.js"></script>
-        <script type="application/javascript">
-            $(document).ready(function () {
-                loadDailyAvail();
-                document.getElementById("numWeek").innerHTML = localStorage.getItem("week");
-                document.getElementById("activityName").innerHTML = localStorage.getItem("name");
-                var day = localStorage.getItem("day");
-                switch (day) {
-                    case '1':
-                        day = 'Monday';
-                        break;
-                    case '2':
-                        day = 'Tuesday';
-                        break;
-                    case '3':
-                        day = 'Wednesday';
-                        break;
-                    case '4':
-                        day = 'Thursday';
-                        break;
-                    case '5':
-                        day = 'Friday';
-                        break;
-                    case '6':
-                        day = 'Saturday';
-                        break;
-                    case '7':
-                        day = 'Sunday';
-                        break;
-                }
-                document.getElementById("dayName").innerHTML = day;
-            })
-          function review(){
-            var button = document.getElementById('confirm');
-            button.style.display='none';
-            var tab = document.getElementById('column');
-            tab.style.display='none';
-            var x = document.getElementById('send');
-            x.style.display="block";
-          }
-        </script>
+<!-- jQuery -->
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.0/jquery.min.js"></script>
 
-      </body>
-      </html>
+<!-- Custom JS -->
+<script type="text/javascript" src="../controllers/loader.js"></script>
+<script type="application/javascript">
+	$(document).ready(function () {
+		loadDailyAvail();
+		loadSelected();
+		document.getElementById("numWeek").innerHTML = localStorage.getItem("week");
+		document.getElementById("activityName").innerHTML = localStorage.getItem("name");
+		var day = localStorage.getItem("day");
+		switch (day) {
+			case '1':
+			day = 'Monday';
+			break;
+			case '2':
+			day = 'Tuesday';
+			break;
+			case '3':
+			day = 'Wednesday';
+			break;
+			case '4':
+			day = 'Thursday';
+			break;
+			case '5':
+			day = 'Friday';
+			break;
+			case '6':
+			day = 'Saturday';
+			break;
+			case '7':
+			day = 'Sunday';
+			break;
+		}
+		document.getElementById("dayName").innerHTML = day;
+	})
+	function review(){
+		var button = document.getElementById('confirm');
+		button.style.visibility='hidden';
+		var tab = document.getElementById('maint-availab');
+		tab.style.visibility='hidden';
+		var x = document.getElementById('send');
+		x.style.visibility="visible";
+	}
+	function changeColor(button){
+		button.style.color="green";
+		var confirm = document.getElementById('confirm');
+		confirm.disabled=false;
+	}
+</script>
+
+</body>
+</html>
 
 
 
