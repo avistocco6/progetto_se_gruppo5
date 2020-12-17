715d84daa44dbdf3d804bc481fcf910c38c68df7
SimonaSorgente <73382491+SimonaSorgente@users.noreply.github.com>
2020-12-17 16:50:50 +0100

    comment

--- src/views/prova.php
+++ src/views/prova.php
@@ -1,38 +1,39 @@
 <?php
-    include_once '..\PgConnection.php';
-    header('Content-Type: application\json');
-    if(isset($_POST['user'])){
+//php code to send email to maintainer and to production manager
+include_once '..\PgConnection.php';
+header('Content-Type: application\json');
+if(isset($_POST['user'])){
 
-        $maintainer =$_POST['user'];
-        echo("il nome del manutentore è". $maintainer ); 
-    } 
+  $maintainer =$_POST['user'];
+  echo("il nome del manutentore è". $maintainer ); 
+} 
 
-   function getemailmain(){
+function getemailmain(){
 
-    $connector = new PgConnection();
-    $conn = $connector->connect();
+  $connector = new PgConnection();
+  $conn = $connector->connect();
 
-    if($conn == null) {
-      return false;
-    }
+  if($conn == null) {
+    return false;
+  }
 
-    $username =$_POST['user'];
-    echo("il nome del manutentore è". $username ); 
-    $emailmain = $connector->query("SELECT email FROM Client WHERE username = " . "'". $username . "'");
-    $row=pg_fetch_row($emailmain)[0];
-    return $row;
-   }
-   function getemailproman(){
+  $username =$_POST['user'];
+  echo("il nome del manutentore è". $username ); 
+  $emailmain = $connector->query("SELECT email FROM Client WHERE username = " . "'". $username . "'");
+  $row=pg_fetch_row($emailmain)[0];
+  return $row;
+}
+function getemailproman(){
 
-    $connector = new PgConnection();
-    $conn = $connector->connect();
+  $connector = new PgConnection();
+  $conn = $connector->connect();
 
-    if($conn == null) {
-      return false;
-    }
+  if($conn == null) {
+    return false;
+  }
 
-    $emailpro = $connector->query("SELECT email FROM Client WHERE username = 'ProManager'");
-    $row=pg_fetch_row($emailpro)[0];
-    return $row;
-   }
+  $emailpro = $connector->query("SELECT email FROM Client WHERE username = 'ProManager'");
+  $row=pg_fetch_row($emailpro)[0];
+  return $row;
+}
 ?>
